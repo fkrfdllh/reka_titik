fde1a5e5cc90088d6ecc763d25d141b465ee744c
Fikri Fadillah <fkrfdllh@gmail.com>
2021-10-25 20:33:05 +0700

    first upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let scrollToElement = (el, ms) => {
+    let speed = (ms) ? ms : 600;
+
+    $("html,body").animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+}
+$(document).ready(function () {
+    const banner = document.getElementsByClassName('ui fluid image');
+    new simpleParallax(banner);
+})
+
+$("#aboutBtn").on('click', function () {
+    scrollToElement("#about", 1200);
+});
+
+$("#productBtn").on('click', function () {
+    scrollToElement("#product", 1200);
+});
