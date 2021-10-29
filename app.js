7d29166ea8a363c51c2f810e0d10f160b4267dbf
Fikri Fadillah <fkrfdllh@gmail.com>
2021-10-29 20:19:31 +0700

    yoho

--- app.js
+++ app.js
@@ -5,21 +5,36 @@ let scrollToElement = (el, ms) => {
         scrollTop: $(el).offset().top
     }, speed);
 }
+
 $(document).ready(function () {
     const banner = document.getElementsByClassName('ui fluid image');
     new simpleParallax(banner);
 })
 
-$("#aboutBtn").on('click', function () {
+$(".homeBtn").on('click', function () {
+    scrollToElement('#top', 1200);
+})
+
+$(".aboutBtn").on('click', function () {
     scrollToElement("#about", 1200);
 });
 
-$("#productBtn").on('click', function () {
+$(".productBtn").on('click', function () {
     scrollToElement("#product", 1200);
 });
 
-$("#contactBtn").on('click', function () {
+$(".contactBtn").on('click', function () {
     scrollToElement("#contact", 1200);
 });
 
 $(".ui.embed").embed({source: 'youtube'});
+
+$("#navbar").visibility({
+    once: false,
+    onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+    },
+    onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+    }
+});
